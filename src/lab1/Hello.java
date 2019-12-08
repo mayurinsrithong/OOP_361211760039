187b269045d59e8e9268061bc5f851a84fe1f482
mild <mayurin0039@gmail.com>
2019-12-08 11:16:32 +0700

    Add message

---
+++ src/lab1/Hello.java
@@ -0,0 +1,22 @@
+package lab1;
+
+public class Hello {
+    public static void main(String[] args) {
+
+     System.out.println("Hello World,I am mild");
+     System.out.println("MIT");
+     System.out.println("Faculty of Management Technology");
+
+
+
+
+
+
+
+
+
+
+
+    }//maim
+
+}//class
